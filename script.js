a93bd4feb2a1a6036ee4a42806e110a9334ba1ba
Lucca <cerasommalucca@gmail.com>
2025-04-01 08:22:07 -0300

    funcao para recuperar inputs e numero aleatorio

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+//world variables 
 let qtd_choice = document.querySelector("#qtd_choice")
+let min_number = document.querySelector("#min")
+let max_number = document.querySelector("#max")
 
-qtd_choice.keyPress() => {
-    
+qtd_choice.oninput = () =>{
+   console.log(qtd_choice.value)
 }
+
+min_number.oninput = () =>{
+   console.log(qtd_choice.value)
+}
+
+max_number.oninput = () =>{
+   console.log(qtd_choice.value)
+}
+
+//funcao para pegar numeros randoon
+
+function getRandonNumber(min,max){
+   Math.floor(Math.random(), (min - max + 1)) + min
+}
+
+
+qtd_choice.addEventListener("click", function() {
+   console.log(qtd_choice.textContent)
+  });
